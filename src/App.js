f5713a64b2eb300608b976e598817dfe3d8beea3
Danimal <danimal@hackpoetic.com>
2023-10-27 13:05:09 -0400

    css reset

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import styled from "styled-components";
 import DocumentationView from "./views/DocumentationView";
 import TimersView from "./views/TimersView";
 
+// CSS Reset
+import "the-new-css-reset/css/reset.css";
+
 const Container = styled.div`
   background: #f0f6fb;
   height: 100vh;
